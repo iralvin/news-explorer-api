250bbafabbe96608b3315cfb3b0399b473073f56
alvin <alvinkwong1@gmail.com>
2020-12-18 21:39:01 -0500

    fix headers

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.options('*', cors());
 app.use(cors());
 app.all('*', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
 });
 
